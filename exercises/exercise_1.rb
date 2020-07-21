cfb3b282cc544ad5b69808a0dfd9687193c5c800
Sophie <sophiemargueritedubois@gmail.com>
2020-07-21 01:16:27 +0000

    completed exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,17 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+class Store < ActiveRecord::Base
+  
+end
+
+Store.create(name: "Burnaby", annual_revenue: 3000000, mens_apparel: true, womens_apparel: true);
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true);
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false);
+
+puts Store.count
+
+
+
+
 # Your code goes below here ...
