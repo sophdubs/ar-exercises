0529202d5f61869600c3872f9b154ea42ae9b2d1
Sophie <sophiemargueritedubois@gmail.com>
2020-07-21 01:38:31 +0000

    Completed exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,7 +4,6 @@ puts "Exercise 1"
 puts "----------"
 
 class Store < ActiveRecord::Base
-  
 end
 
 Store.create(name: "Burnaby", annual_revenue: 3000000, mens_apparel: true, womens_apparel: true);
@@ -12,8 +11,3 @@ Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, wom
 Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false);
 
 puts Store.count
-
-
-
-
-# Your code goes below here ...
